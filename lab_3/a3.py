4be71eeeb565919fceeb3ffa6e06116af6513d44
gulinskii <gulinskij_nd_25@student.grsu.by>
2025-11-04 10:33:52 +0300

    Update a3.py

--- lab_3/a3.py
+++ lab_3/a3.py
@@ -1,5 +1,3 @@
-from decimal import Decimal, ROUND_HALF_UP
-
 wased = int(input("Введите показания с счетчика за прошлый месяц: "))
 new = int(input("Введите показания счетчика за текущий месяц: "))
 
@@ -15,27 +13,24 @@ if result <= 300:
 
 elif result > 300 and result <= 600:
     price = 21 + (result - 300) * 0.06
-    price_rounded = Decimal(str(price)).quantize(Decimal('0.01'), rounding=ROUND_HALF_UP) #для более точного округления
     price_double = price / result
 
     print("Предыдущее: {0}\nТекущее: {1}\nИспользовано: {2}\nК оплате: {3}\nСр. цена m^3: {4:.2f}"
-    .format(wased, new, result, price_rounded, price_double))
+    .format(wased, new, result, price, price_double))
 
 elif result > 600 and result <= 800:
     x = result - 300
     y = x - 300
 
     price = 21 + (y * 0.04) + (300 * 0.06)
-    price_rounded = Decimal(str(price)).quantize(Decimal('0.01'), rounding=ROUND_HALF_UP)
     price_double = price / result
     
     print("Предыдущее: {0}\nТекущее: {1}\nИспользовано: {2}\nК оплате: {3}\nСр. цена m^3: {4:.2f}"
-    .format(wased, new, result, price_rounded, price_double))
+    .format(wased, new, result, price, price_double))
 
 else:
     price = 21 + (300 * 0.06) + (200 * 0.04) + (result - 300 - 200 - 300) * 0.025
-    price_rounded = Decimal(str(price)).quantize(Decimal('0.01'), rounding=ROUND_HALF_UP)
     price_double = price / result
     
     print("Предыдущее: {0}\nТекущее: {1}\nИспользовано: {2}\nК оплате: {3}\nСр. цена m^3: {4:.2f}"
-    .format(wased, new, result, price_rounded, price_double))
+    .format(wased, new, result, price, price_double))
